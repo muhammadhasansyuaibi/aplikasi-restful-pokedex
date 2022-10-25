c83010ab00b88411e72a42f096b1ab0690197371
muhammadhasansyuaibi <muhammadhasansyuaibi@gmail.com>
2022-10-25 23:09:32 +0700

    commit pertama gess, page all pokemon

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      myBlack: "#252525",
+      hoverText: "#A5A5A5",
+      grey: "#DBDCDC",
+      grass: "#9BCC50",
+      poison: "#B97FC9",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
